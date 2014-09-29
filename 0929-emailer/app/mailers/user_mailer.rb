ea4a69ce1e1a37648977979cea3f2654242372a4
Adam Carter <adamrcarter@gmail.com>
2014-09-29 17:16:18 -0500

    WIP

--- 0929-emailer/app/mailers/user_mailer.rb
+++ 0929-emailer/app/mailers/user_mailer.rb
@@ -1,18 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "adamcarterdev@gmail.com"
-  
-  def welcome(user)
-    @user = user
-    mail(:to => user.email, :subject => "welcome, dear human")
-  end
-    
-    def task(user)
+
+    def save_message(user)
       @user = user
-      mail(:to => user.email, :subject => "You have been assigned a task")
-    end
-    
-    def update_task(user)
-      @user = user
-      mail(:to => user.email, :subject => "A Task has been updated")   
+      mail(:to => user.email, :subject => "New message")   
   end
 end
